361771521813f49c2001b6ab57f53edad339529e
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-02 09:11:03 -0500

    python: day 2, part 1 complete

---
+++ day-02/python/part1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import sys
+
+file = open(sys.argv[1], "r")
+total = 0
+letterMatch = 0
+
+for line in file:
+    param = line.split()
+    passRange = param[0].split("-")
+    targetLetter = param[1][0]
+    password = param[2]
+
+    letterMatch = password.count(targetLetter)
+    if letterMatch >= int(passRange[0]) and letterMatch <= int(passRange[1]):
+            total += 1
+            
+print(total)
