51bef626aa60ecf2ae399a688de00e8e15a1f10a
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-03 09:56:08 -0500

    python: day 3 complete

---
+++ day-03/python/part1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+
+data = [x.strip() for x in open(sys.argv[1], 'r').readlines()]
+
+slope = (3, 1)  # right, down
+
+numRows = len(data)  # changes between the example and actual input
+numCols = len(data[0])  # ^
+
+# m = y2 - y1 / x2 - x1 -> basically y/x
+
+trees = 0
+x = 0
+
+for y in range(0, numRows, slope[1]):  # -> step right by factor of right 
+    if data[y][x] == '#':  # this is not a 2d array, it is checking the y and x-axis for a "tree"
+        trees += 1
+    x = (x + slope[0]) % numCols  # allow for "looping" around on the x-axis
+    
+print(f"trees: {trees}")
+
+
