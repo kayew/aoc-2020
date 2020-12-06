8ebd0dee6c7b30d4a3ef7832a55a4c7116ebb282
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-06 17:39:06 -0500

    day 6, nim part 2: reduce line length

--- day-06/nim/part2.nim
+++ day-06/nim/part2.nim
@@ -6,7 +6,7 @@ let data = paramStr(1).string.readfile.split("\n\n")
 var total = 0
 
 for q in data:
-    var u: HashSet[char] = toHashSet(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'])
+    var u = toHashSet(['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'])
     for c in q.split():
         u = u * toHashSet(c)
 
