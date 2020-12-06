8faabd4d785ed739b5429a5879fff922ff4acbcd
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-06 12:36:30 -0500

    day 6, python: show what is actually being done in parts 1 and 2

--- day-06/python/part1.py
+++ day-06/python/part1.py
@@ -2,14 +2,14 @@
 
 import sys
 
-data = [x.replace("\n", "") for x in open(sys.argv[1]).read().split("\n\n")]
+data = [x.split() for x in open(sys.argv[1]).read().split("\n\n")]
 
 total = 0
 
 for q in data:
     u = set()
     for c in q:
-        u.add(c)
+        u = u.union(set(c))
     
     total += len(u)
     
--- day-06/python/part2.py
+++ day-06/python/part2.py
@@ -2,13 +2,13 @@
 
 import sys, string
 
-data = [x for x in open(sys.argv[1]).read().split("\n\n")]
+data = [x.split() for x in open(sys.argv[1]).read().split("\n\n")]
 
 total = 0
 
 for q in data:
     u = set(string.ascii_lowercase)
-    for c in q.split():
+    for c in q:
         u = u.intersection(set(c))
     
     total += len(u)
