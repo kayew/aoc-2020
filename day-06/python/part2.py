1d89d038491eee37b6e4cc99c4e522a370b88712
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-06 10:58:18 -0500

    day 6, python part 2: make imports one line

--- day-06/python/part2.py
+++ day-06/python/part2.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-import sys
-import string
+import sys, string
 
 data = [x for x in open(sys.argv[1]).read().split("\n\n")]
 
 total = 0
 
 for q in data:
-    u = set([x for x in string.ascii_lowercase])
+    u = set(string.ascii_lowercase)
     for c in q.split():
-        u = u.intersection(set([x for x in c]))
+        u = u.intersection(set(c))
     
     total += len(u)
     
