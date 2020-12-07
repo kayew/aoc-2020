ccd519b1f5d4b9cd334f4857165c935ec7ad33fe
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-07 18:25:51 -0500

    day 7, python: part 1 complete. fuck you part 1!

--- day-07/python/part1.py
+++ day-07/python/part1.py
@@ -1,23 +1,42 @@
 #!/usr/bin/env python3
 
 import sys
-import re
 
 # fmt: off
 data = [x.strip().split(" bags contain ") for x in open(sys.argv[1]).readlines()]
 
+d = {}
+
 for b in data:
-    d = {}
     d[b[0]] = b[1].split(", ")
-    for k, v in d.items():
-        t = []
-        for i in range(len(v)):
-            if v[i][0] != "n":
-                t.append(v[i][2:].split(" bag")[0])
-                d[k] = t
-            else:
-                d[k] = []
-                continue
 
-print(d)
+for k,v in d.items():
+    t = []
+    for i in v:
+        if i[0] != "n":
+            t.append(i[2:].split(" bag")[0])
+            d[k] = t
+        else:
+            d[k] = []
+            continue
 
+def can_contain():
+    total = 0
+    for sb in d:
+        total += check_sub_bag(sb)
+    return total
+
+def check_sub_bag(sub):
+    total = 0
+    if sub == "shiny gold":
+        return 1
+    for l in d[sub]:
+        if l == "shiny gold": 
+            total += 1
+        else:
+            ans = check_sub_bag(l)
+            if ans > 0:
+                return ans
+    return total
+
+print(can_contain()-1)
