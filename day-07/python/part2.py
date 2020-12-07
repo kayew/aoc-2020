05ea01c114a7b2005f6807d899a3da374e99315a
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-07 16:27:48 -0500

    day 7, python: partial completeion, parsing complete

---
+++ day-07/python/part2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+# fmt: off
+data = [x.strip().split(" bags contain ") for x in open(sys.argv[1]).readlines()]
+
+for b in data:
+    d = {}
+    d[b[0]] = b[1].split(", ")
+    for k, v in d.items():
+        t = []
+        for i in range(len(v)):
+            if v[i][0] != "n":
+                t.append((v[i][2:].split(" bag")[0], int(v[i][0])))
+                d[k] = t
+            else:
+                d[k] = []
+                continue
+
+    print(d)
+
+
