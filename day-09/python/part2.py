378cb890b7d259f56f52bbb0b21f341612372f87
Sebastian Alejandro <sebastian1231alejandro@gmail.com>
2020-12-09 13:34:52 -0500

    day 9, python part 2: j+1 to avoid extra checking

--- day-09/python/part2.py
+++ day-09/python/part2.py
@@ -17,20 +17,16 @@ def find_invalid(step, data):
             return data[i]
 
 invalid_num = find_invalid(step, data)
-# print(invalid_num)
 
 s = []
 
 for i in range(len(data)):
-    for j in range(i, len(data)):
-        if sum(data[i:j]) == invalid_num:
-            for x in data[i:j]:
-                if x == invalid_num:
-                    continue
-                else:   
-                    s.append(x)
+    for j in range(i+1, len(data)):
+        if sum(data[i:j+1]) == invalid_num:
+            for x in data[i:j+1]:
+                s.append(x)
             break
-        if sum(data[i:j]) > invalid_num:
+        if sum(data[i:j+1]) > invalid_num:
             break
 
 s.sort()
